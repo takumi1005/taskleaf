8c1a55aa002f9f2e5e0c0dece5ca3efb5a033175
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2019-08-21 15:24:56 +0900

    task

---
+++ app/models/task.rb
@@ -0,0 +1,18 @@
+class Task < ApplicationRecord
+  before_validation :set_unless_name
+  belongs_to :user
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+
+  private
+
+  def set_unless_name
+    self.name = '名前なし' if name.blank?
+  end
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることは出来ません') if name&.include?(',')
+  end
+
+end
